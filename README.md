bbe93ae8855e472911c29ab50fdc39c657ec15c0
Mark <mverleg.noreply@gmail.com>
2020-10-18 21:18:59 +0200

    Mock database

--- README.md
+++ README.md
@@ -6,8 +6,16 @@ This is a small demo for how to set up your configuration so that you can run yo
 * Kubernetes: I'll use minikbue for a local kubernetes cluster for testing
 * Database: I'll use a simple python script to test reachability.
 
+You will need Python 3, Docker and Kubernetes (i.e. minikube).
 
+## Steps
 
+* Build the images:
+
+    
+* Start the 'mock database':
+
+    python3 mock_db.py localhost 1234
 
 
 
---
+++ mock_db.py
@@ -0,0 +1,43 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from json import dumps
+
+
+class MockDB(BaseHTTPRequestHandler):
+    def _set_headers(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
+
+    def do_GET(self):
+        self._set_headers()
+        self.wfile.write(dumps(dict(
+            status='you have reached the mock database',
+        )).encode('utf-8'))
+
+    def do_HEAD(self):
+        self._set_headers()
+
+
+def run(host, port):
+    httpd = HTTPServer((host, port), MockDB)
+    print('Starting httpd...')
+    httpd.serve_forever()
+
+
+if __name__ == "__main__":
+    from sys import argv
+
+    if len(argv) < 2:
+        print('first argument should be the bind host')
+        exit(1)
+    if len(argv) < 3:
+        print('second argument should be the bind port')
+        exit(1)
+    try:
+        port = int(argv[2])
+    except ValueError:
+        print('second argument should be the bind port, which must be a positive integer')
+        exit(1)
+
+    run(argv[1], int(argv[2]))
+
